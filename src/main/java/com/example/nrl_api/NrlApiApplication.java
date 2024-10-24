7ca52c785d18f8c3659842b06d19ce9b889ec13e
CMoore002 <curtis.moore205@gmail.com>
2024-10-24 19:01:18 +1100

    initial commit

---
+++ src/main/java/com/example/nrl_api/NrlApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.nrl_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NrlApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NrlApiApplication.class, args);
+	}
+
+}
