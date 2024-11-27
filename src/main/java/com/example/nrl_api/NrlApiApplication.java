815f6e017c55781ff059f90d8d963f63fa54f04d
CMoore002 <curtis.moore205@gmail.com>
2024-11-27 22:43:38 +1100

    Added Dockerfile Added docker-compose Changed ports

--- src/main/java/com/example/nrl_api/NrlApiApplication.java
+++ src/main/java/com/example/nrl_api/NrlApiApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class NrlApiApplication {
 	public static void main(String[] args) {
-
 		SpringApplication.run(NrlApiApplication.class, args);
 	}
 
